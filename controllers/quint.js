649cd078ab22696c38b481ad53fbc823a8342305
Alvaluis01 <luialvarado@uniboyaca.edu.co>
2023-11-02 10:43:38 -0500

    html, css, js en el 5°terminad,con mejors otrs pts

---
+++ controllers/quint.js
@@ -0,0 +1,18 @@
+//<meta charset="UTF-8"></meta>
+function identificarNumero() {
+    const inputNumero = document.getElementById("numero");
+    const resultado = document.getElementById("resultado");
+    const numero = parseFloat(inputNumero.value);
+
+    if (Number.isInteger(numero) && numero > 0) {
+        if (numero % 2 === 0) {
+            resultado.textContent = "Par";
+        } else {
+            resultado.textContent = "Impar";
+        }
+    } else if (!Number.isInteger(numero)) {
+        resultado.textContent = "Debe ingresar números enteros, no decimales.";
+    } else if (numero <= 0) {
+        resultado.textContent = "Debe ingresar números positivos, no negativos.";
+    }
+}
